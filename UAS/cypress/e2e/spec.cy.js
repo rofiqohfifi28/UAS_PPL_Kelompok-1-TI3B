ecd4addf0b27c680dbaedafc42936a7f78801d32
rofiqohfifi28 <99854026+rofiqohfifi28@users.noreply.github.com>
2022-12-19 16:41:15 +0700

    Add files via upload

---
+++ UAS/cypress/e2e/spec.cy.js
@@ -0,0 +1,59 @@
+
+//LOGIN
+
+//login berhasil( username dan password benar)
+/*describe('login', () => {
+  it('passes', () => {
+ 
+    cy.visit('http://localhost/proyek2nya/admin/login.php')
+    cy.get(':nth-child(1) > .form-control').type('jasmin')
+    cy.get(':nth-child(2) > .form-control').type('jasmin')
+    cy.get('.btn-success').click()    
+  })
+})
+*/
+//login gagal (username salah password benar)
+
+/*describe('login', () => {
+  it('passes', () => {
+ 
+    cy.visit('http://localhost/proyek2nya/admin/login.php')
+    cy.get(':nth-child(1) > .form-control').type('rofiqoh')
+    cy.get(':nth-child(2) > .form-control').type('jasmin')
+    cy.get('.btn-success').click()    
+  })
+
+})
+*/
+
+//login gagal (username benar password salah)
+describe('login', () => {
+  it('passes', () => {
+ 
+    cy.visit('http://localhost/proyek2nya/admin/login.php')
+    cy.get(':nth-child(1) > .form-control').type('jasmin')
+    cy.get(':nth-child(2) > .form-control').type('rofiqoh')
+    cy.get('.btn-success').click()    
+  })
+})
+
+//TAMBAH DATA
+//tambah data berhasil 
+describe('login', () => {
+  it('passes', () => {
+ 
+    cy.visit('http://localhost/proyek2nya/admin/login.php')
+    cy.get(':nth-child(1) > .form-control').type('jasmin')
+    cy.get(':nth-child(2) > .form-control').type('jasmin')
+    cy.get('.btn-success').click()    
+    cy.get(':nth-child(1) > [data-toggle="modal"]').click()
+    cy.get(':nth-child(1) > #exampleInputEmail1').type('Rahma Nur Samdari')
+    cy.get('#exampleModalCenter > .modal-dialog > .modal-content > .modal-body > form > :nth-child(2) > #exampleInputEmail1').type('RahmaNur')
+    cy.get('#exampleModalCenter > .modal-dialog > .modal-content > .modal-body > form > :nth-child(3) > #exampleInputEmail1').type('siswa3')
+    cy.get('form > select.form-control').select('Kelas 7')
+    cy.get('#exampleModalCenter > .modal-dialog > .modal-content > .modal-body > form > :nth-child(6) > .form-control').type('Jln.Ronggolawe 14 Malang')
+    cy.get('#exampleModalCenter > .modal-dialog > .modal-content > .modal-body > form > :nth-child(7) > #exampleInputEmail1').type('08123531689')
+    cy.get('#exampleModalCenter > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
+  })
+})
+
